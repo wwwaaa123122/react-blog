e523b9d7f05e917b1eba25c0364851a92606acb1
wwwaaa123122 <i@mcxclr.top>
2026-08-15 22:56:24 +0800

    feat: 添加 /gh /bot /rss 跳转路由（从 Firefly 迁移）

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Posts from "./pages/Posts";
@@ -11,6 +11,12 @@ import NotFound from "./pages/NotFound";
 export default function App() {
   return (
     <Routes>
+      {/* 站外跳转（不经过 Layout） */}
+      <Route path="/gh" element={<Navigate to="https://github.com/wwwaaa123122" replace />} />
+      <Route path="/bot" element={<Navigate to="https://xc.bot.cd/" replace />} />
+      <Route path="/rss" element={<Navigate to="/rss.xml" replace />} />
+
+      {/* 主站页面 */}
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<Posts />} />
