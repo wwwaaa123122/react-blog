e29f40dba594050d00a9cbe3d2ea242474ab35ed
wwwaaa123122 <i@mcxclr.top>
2026-08-15 22:49:18 +0800

    fix: 移除未使用的 useRef 导入

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
